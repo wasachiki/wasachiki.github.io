f2fb09bb7014d1bc01bfdbcf0c383d72211b0aa4
wasachiki <toyadondon@gmail.com>
2022-05-29 22:24:31 +0900

    <commit message>

--- works/counter.js
+++ works/counter.js
@@ -1,6 +1,13 @@
 let count = 0
 const countUp = () => {
   count++
+  countReload()
+}
+const countReset = () => {
+  count=0
+  countReload()
+}
+const countReload = () => {
   const countElement = document.querySelector('#count')
   countElement.innerText = '回数: ' + count
 }
